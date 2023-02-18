afa31f5cadaf62b1e4d38586c17af6884e53f82a
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-02-18 07:42:33 +0100

    make post model fields nullable to avoid warnings

--- services/post/Models/Post.cs
+++ services/post/Models/Post.cs
@@ -6,10 +6,10 @@ namespace post.Models
 
         [Required(ErrorMessage = "Title is required")]
         [MinLength(3, ErrorMessage = "Title must be at least 3 characters long")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         [Required(ErrorMessage = "Content is required")]
         [MinLength(10, ErrorMessage = "Content must be at least 10 characters long")]
-        public string Content { get; set; }
+        public string? Content { get; set; }
     }
 }
