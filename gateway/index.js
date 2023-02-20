bd7647e40736c882ff1bc7b450d749ddddfccd78
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-02-20 05:38:20 +0100

    add user service to api gateway

--- gateway/index.js
+++ gateway/index.js
@@ -6,11 +6,19 @@ const app = express();
 const port = 3000;
 
 // proxies
+const userProxy = httpProxy.createProxyServer({
+  target: 'http://user_service'
+});
+
 const postProxy = httpProxy.createProxyServer({
   target: 'http://post_service'
 });
 
 // routes
+app.all('/api/users/*', (req, res) => {
+  userProxy.web(req, res);
+});
+
 app.all('/api/posts/*', (req, res) => {
   postProxy.web(req, res);
 });
