c9e756bf5b958c0f4e3ce5ca6bcc654793a6f054
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-02-20 04:12:53 +0100

    add post service to api gateway

--- gateway/index.js
+++ gateway/index.js
@@ -5,6 +5,16 @@ const httpProxy = require('http-proxy');
 const app = express();
 const port = 3000;
 
+// proxies
+const postProxy = httpProxy.createProxyServer({
+  target: 'http://post_service'
+});
+
+// routes
+app.all('/api/posts/*', (req, res) => {
+  postProxy.web(req, res);
+});
+
 // start server
 app.listen(port, () => {
   console.log(`Gateway listening at http://localhost:${port}`);
